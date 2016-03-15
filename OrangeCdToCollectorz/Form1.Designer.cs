8e21a34145c5b9270d62d0c58c2d6c29071bbd5b
Tom Schulte <tsch@plex.com>
2016-03-14 22:57:12 -0400

    alpha version

---
+++ OrangeCdToCollectorz/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DelDupes
+{
+  internal partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+      this.button1 = new System.Windows.Forms.Button();
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this.checkBoxDoIt = new System.Windows.Forms.CheckBox();
+      this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+      this.textSuffix = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // openFileDialog2
+      // 
+      this.openFileDialog2.FileName = "openFileDialog2";
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(193, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Find Target Folder";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.Location = new System.Drawing.Point(13, 111);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.Size = new System.Drawing.Size(843, 420);
+      this.richTextBox1.TabIndex = 1;
+      this.richTextBox1.Text = "";
+      this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(251, 11);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(605, 23);
+      this.progressBar1.TabIndex = 2;
+      // 
+      // backgroundWorker1
+      // 
+      this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+      // 
+      // checkBoxDoIt
+      // 
+      this.checkBoxDoIt.AutoSize = true;
+      this.checkBoxDoIt.Location = new System.Drawing.Point(13, 60);
+      this.checkBoxDoIt.Name = "checkBoxDoIt";
+      this.checkBoxDoIt.Size = new System.Drawing.Size(141, 17);
+      this.checkBoxDoIt.TabIndex = 3;
+      this.checkBoxDoIt.Text = "Delete (No Confirmation)";
+      this.checkBoxDoIt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.checkBoxDoIt.UseVisualStyleBackColor = true;
+      // 
+      // folderBrowserDialog1
+      // 
+      this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+      // 
+      // textSuffix
+      // 
+      this.textSuffix.Location = new System.Drawing.Point(238, 60);
+      this.textSuffix.Name = "textSuffix";
+      this.textSuffix.Size = new System.Drawing.Size(100, 20);
+      this.textSuffix.TabIndex = 4;
+      this.textSuffix.Text = ".1";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(354, 60);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(31, 13);
+      this.label1.TabIndex = 5;
+      this.label1.Text = "suffix";
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(868, 543);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.textSuffix);
+      this.Controls.Add(this.checkBoxDoIt);
+      this.Controls.Add(this.progressBar1);
+      this.Controls.Add(this.richTextBox1);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.RichTextBox richTextBox1;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    private System.Windows.Forms.CheckBox checkBoxDoIt;
+    private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    private System.Windows.Forms.TextBox textSuffix;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ OrangeCdToCollectorz/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DelDupes
+{
+  internal partial class Form1 : Form
+  {
+    string _lastPoprerty = string.Empty;
+    string _selectedPath = string.Empty;
+
+
+    // This delegate enables asynchronous calls for setting  
+    // the text property on a TextBox control.  
+    delegate void SetTextCallback(RichTextBox label, string text);
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+
+      DialogResult result = folderBrowserDialog1.ShowDialog();
+      if (result == DialogResult.OK) // Test result.
+      {
+        this._selectedPath = folderBrowserDialog1.SelectedPath;
+        richTextBox1.Text = "Loading contents of " + this._selectedPath + Environment.NewLine;
+
+        if (this.checkBoxDoIt.Checked == false)
+        {
+          richTextBox1.Text = "WILL NOT DELETE" + Environment.NewLine;
+        }
+
+        progressBar1.Maximum = 100;
+        progressBar1.Step = 1;
+        progressBar1.Value = 0;
+        backgroundWorker1.WorkerReportsProgress = true;
+        backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker_ProgressChanged);
+        backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker_RunWorkerCompleted);
+        backgroundWorker1.RunWorkerAsync();
+      }
+      else
+      {
+        richTextBox1.Text = result.ToString(); // <-- For debugging use.
+      }
+    }
+
+    private void Form1_Load(object sender, System.EventArgs e)
+    {
+      // Start the BackgroundWorker.
+      backgroundWorker1.RunWorkerAsync();
+    }
+
+    private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      this.progressBar1.Value = e.ProgressPercentage;
+      this.progressBar1.Update();
+    }
+
+    private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      // write output to a file
+      richTextBox1.Text += Environment.NewLine + "COMPLETE!";
+    }
+
+    private void DirSearch(string sDir, object sender, DoWorkEventArgs e)
+    {
+      var backgroundWorker = sender as BackgroundWorker;      // Show the dialog and get result.
+      //int albumCnt = orangeCdCollection.Albums.All.Length;
+      //this.SetText(this.richTextBox1, "OrangeCd.Collection has " + albumCnt.ToString() + " albums." + Environment.NewLine);
+
+      ////richTextBox1.Text += "OrangeCd.Collection has " + albumCnt.ToString() + " albums." + Environment.NewLine;
+      //int cnt = 0;
+
+      try
+      {
+        //cnt++;
+        //TODO: Get progress bar to work
+        //double progress = cnt * 100.0 / albumCnt;
+        //backgroundWorker.ReportProgress((int)progress);
+
+        //Directory.GetFiles(dir, "*" + this.textSuffix.Text + ".*")
+
+        string searchPattern = "*" + this.textSuffix.Text + ".*";
+
+        foreach (string file in Directory.GetFiles(sDir, searchPattern))
+        {
+          FileInfo fileInfo = new FileInfo(file);
+          long length = fileInfo.Length;
+          string name = fileInfo.Name;
+          string extension = fileInfo.Extension;
+
+          string targetName = name.Replace(this.textSuffix.Text + fileInfo.Extension, "") + fileInfo.Extension;
+
+          string path = Path.Combine(fileInfo.DirectoryName, targetName);
+
+          if (File.Exists(path))
+          {
+            FileInfo fileInfoTarget = new FileInfo(path);
+            if (fileInfoTarget.Length == length)
+            {
+              this.SetText(this.richTextBox1, Environment.NewLine);
+              this.SetText(
+                this.richTextBox1,
+                string.Format(
+                  "Found file {0} with same size of {1} as file {2}",
+                  file,
+                  length.ToString(),
+                  fileInfoTarget.Name));
+              this.SetText(this.richTextBox1, Environment.NewLine);
+
+              if (this.checkBoxDoIt.Checked == true)
+              {
+                File.Delete(path);
+                this.SetText(this.richTextBox1, "DELETED: " + file + Environment.NewLine);
+              }
+            }
+          }
+          else
+          {
+            this.SetText(
+              this.richTextBox1,
+              string.Format(
+                "File {0} with size of {1} has no matching file {2}",
+                file,
+                length.ToString(),
+                path));
+            this.SetText(this.richTextBox1, Environment.NewLine);
+          }
+        }
+
+        foreach (string d in Directory.GetDirectories(sDir))
+        {
+          this.DirSearch(d, sender, e);
+        }
+      }
+      catch (System.Exception ex)
+      {
+        this.SetText(this.richTextBox1, "ERROR: " + ex.Message + Environment.NewLine);
+        // listBox1.Items.Add(ex.Message);
+      }
+    }
+
+    private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+    {
+      //var backgroundWorker = sender as BackgroundWorker;      // Show the dialog and get result.
+      //int albumCnt = orangeCdCollection.Albums.All.Length;
+      //this.SetText(this.richTextBox1, "OrangeCd.Collection has " + albumCnt.ToString() + " albums." + Environment.NewLine);
+
+      ////richTextBox1.Text += "OrangeCd.Collection has " + albumCnt.ToString() + " albums." + Environment.NewLine;
+      //int cnt = 0;
+
+      try
+      {
+        //cnt++;
+        //double progress = cnt * 100.0 / albumCnt;
+        //backgroundWorker.ReportProgress((int)progress);
+        DirSearch(this._selectedPath, sender, e);
+      }
+      catch (System.Exception ex)
+      {
+        this.SetText(this.richTextBox1, "ERROR: " + ex.Message + Environment.NewLine);
+        // listBox1.Items.Add(ex.Message);
+      }
+    }
+
+    private void richTextBox1_TextChanged(object sender, EventArgs e)
+    {
+      richTextBox1.SelectionStart = richTextBox1.Text.Length; //Set the current caret position at the end
+      richTextBox1.ScrollToCaret(); //Now scroll it automatically
+    }
+
+    // This method demonstrates a pattern for making thread-safe
+    // calls on a Windows Forms control. 
+    //
+    // If the calling thread is different from the thread that
+    // created the TextBox control, this method creates a
+    // SetTextCallback and calls itself asynchronously using the
+    // Invoke method.
+    //
+    // If the calling thread is the same as the thread that created
+    // the TextBox control, the Text property is set directly. 
+    private void SetText(RichTextBox label, string text)
+    {
+      // InvokeRequired required compares the thread ID of the
+      // calling thread to the thread ID of the creating thread.
+      // If these threads are different, it returns true.
+      if (label.InvokeRequired)
+      {
+        SetTextCallback d = new SetTextCallback(SetText);
+        this.Invoke(d, new object[] { label, text });
+      }
+      else
+      {
+        label.Text += text;
+      }
+    }
+
+    private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
